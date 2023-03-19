8481b256d166071e9e8b70cb1b2ebaf56f2f404e
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-19 14:07:15 +0500

    Tabs container

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,38 +11,52 @@ import { useRouter } from "expo-router";
 import { icons, SIZES } from "../../../constants";
 import styles from "./welcome.style";
 
-const jobTypes = ["Full-time", "Part-time", "Contractor"]
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState("");
+	const router = useRouter();
+	const [searchTerm, setSearchTerm] = useState("");
 	return (
 		<View>
 			<View style={styles.container}>
-        <Text style={styles.userName}>Hello Ahmed!</Text>
-        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
-      </View>
+				<Text style={styles.userName}>Hello Ahmed!</Text>
+				<Text style={styles.welcomeMessage}>
+					Find your perfect job!
+				</Text>
+			</View>
 
-      <View style={styles.searchContainer}>
-        <View style={styles.searchWrapper}>
-          <TextInput 
-            style={styles.searchInput}
-            value={searchTerm}
-            clearButtonMode="while-editing"
-            inputMode="search"
-            placeholder="What are you looking for?"
-            onChangeText={(text)=>{setSearchTerm(text)}}
-          />
-        </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
-          <Image 
-            source={icons.search}
-            resizeMode="contain"
-            style={styles.searchBtnImage}
-          />
-        </TouchableOpacity>
-      </View>
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput
+						style={styles.searchInput}
+						value={searchTerm}
+						clearButtonMode="while-editing"
+						inputMode="search"
+						placeholder="What are you looking for?"
+						onChangeText={(text) => {
+							setSearchTerm(text);
+						}}
+					/>
+				</View>
+				<TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+					<Image
+						source={icons.search}
+						resizeMode="contain"
+						style={styles.searchBtnImage}
+					/>
+				</TouchableOpacity>
+			</View>
 
+			<View style={styles.tabsContainer}>
+				<FlatList
+					data={jobTypes}
+					renderItem={({ item }) => {
+						<TouchableOpacity>
+							<Text>{item}</Text>
+						</TouchableOpacity>;
+					}}
+				/>
+			</View>
 		</View>
 	);
 };
