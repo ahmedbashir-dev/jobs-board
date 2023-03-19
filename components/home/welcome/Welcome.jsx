bc106150ab71392d837baca79c40fce153eab1e1
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-19 14:26:00 +0500

    Welcome screen job search

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -15,6 +15,7 @@ const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
 	const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
 	const [searchTerm, setSearchTerm] = useState("");
 	return (
 		<View>
@@ -47,16 +48,26 @@ const Welcome = () => {
 				</TouchableOpacity>
 			</View>
 
-			<View style={styles.tabsContainer}>
-				<FlatList
-					data={jobTypes}
-					renderItem={({ item }) => {
-						<TouchableOpacity>
-							<Text>{item}</Text>
-						</TouchableOpacity>;
-					}}
-				/>
-			</View>
+			
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
 		</View>
 	);
 };
