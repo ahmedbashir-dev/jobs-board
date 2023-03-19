365ba2060c5d9eb6acfd1e5448220babe55dfc5a
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-19 14:06:44 +0500

    Search bar added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	Image,
+	FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+import styles from "./welcome.style";
 
-import styles from './welcome.style'
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+	return (
+		<View>
+			<View style={styles.container}>
+        <Text style={styles.userName}>Hello Ahmed!</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value={searchTerm}
+            clearButtonMode="while-editing"
+            inputMode="search"
+            placeholder="What are you looking for?"
+            onChangeText={(text)=>{setSearchTerm(text)}}
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+		</View>
+	);
+};
+
+export default Welcome;
