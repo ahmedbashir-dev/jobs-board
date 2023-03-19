a940ef6c6aaf5de21285d347c7d5d76f2240969b
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-19 14:52:02 +0500

    Home screen popular job section

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import { useState } from "react";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	FlatList,
+	ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+	const router = useRouter();
+  const [selectedJob, setSelectedJob] = useState(1);
+	const isLoading = false;
+	const error = false;
 
-export default Popularjobs
+  const handleCardPress = () => {}
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerTitle}>Popular Jobs</Text>
+				<TouchableOpacity>
+					<Text style={styles.headerBtn}>Show All</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator size="large" color={COLORS.primary} />
+				) : error ? (
+					<Text>Something went wrong</Text>
+				) : (
+					<FlatList
+						data={[1,2,3,4]}
+						renderItem={({ item }) => (
+							<PopularJobCard
+								item={item}
+								selectedJob={selectedJob}
+								handleCardPress={handleCardPress}
+							/>
+						)}
+						keyExtractor={(item) => item.job_id}
+						contentContainerStyle={{ columnGap: SIZES.medium }}
+						horizontal
+					/>
+				)}
+			</View>
+		</View>
+	);
+};
+
+export default Popularjobs;
